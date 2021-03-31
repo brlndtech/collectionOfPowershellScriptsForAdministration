dfa4b7a751a3f29712369afe821305d303299b27
Brlndtech <38756128+brlndtech@users.noreply.github.com>
2021-03-31 10:11:42 +0200

    Add files via upload

---
+++ create-local-users-machine-de-rebond.ps1
@@ -0,0 +1,6 @@
+﻿$password = Read-Host "Enter the password value for all the local account : "-AsSecureString
+$users = @('gsb','bva','cge','dsa','fga','fmo','frc','gcu','jpa','jvi','kgi','ldu','lle','mbu','pkh','sma','tvi','yge')
+for ( $i = 0; $i -lt $users.count; $i++)  {
+    New-LocalUser $users[$i] -Password $password -FullName $users[$i] -PasswordNeverExpires
+    Add-LocalGroupMember -Group "Administrators" -Member $users[$i]
+}
